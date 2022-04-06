b3480f7adf00348708091c2815cbee4dd86fa248
Tash Musanhu <tashinga.musanhu@and.digital>
2022-04-06 22:35:09 +0100

    css styling added

--- chef-mexican/src/components/Cart/HeaderCart.js
+++ chef-mexican/src/components/Cart/HeaderCart.js
@@ -19,19 +19,17 @@ export default function HeaderCart() {
 
 	return (
 		<>
-			<div className="bg-red-700 text-white absolute flex right-24 h-14 px-5 border-rose-600 rounded-lg place-items-center space-x-2 ">
+			<div
+				onClick={() => {
+					addModalContent(modalContent);
+					showModal();
+				}}
+				className="hover:bg-slate-800 bg-red-700 text-white absolute flex right-24 h-14 px-5 border-rose-600 rounded-lg place-items-center space-x-2 "
+			>
 				<div className="">
 					<FontAwesomeIcon icon={faCartShopping} className="fa-xl" />
 				</div>
-				<button
-					onClick={() => {
-						addModalContent(modalContent);
-						showModal();
-					}}
-					className="font-semibold"
-				>
-					View Cart
-				</button>
+				<div className="font-semibold">View Cart</div>
 				<div className="bg-red-500 w-8 h-8 rounded-full text-center font-semibold pt-1">
 					<h1 className="">{quantity}</h1>
 				</div>
--- chef-mexican/src/components/UI/ Modal/Modal.js
+++ chef-mexican/src/components/UI/ Modal/Modal.js
@@ -2,30 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useContext } from "react";
 import ModalContext from "../../../Context/ModalContext";
-import CartContext from "../../../Context/CartContext";
 import CartItems from "../../Cart/CartItems";
 
 export default function Modal() {
 	const buttonCss = "py-1 rounded-full w-20 h-10 mt-3 w-24 bg-red-700";
 
-	const { hideModal, modalContent} = useContext(ModalContext);
+	const { hideModal, modalContent } = useContext(ModalContext);
 
-    let cart = modalContent[0];
+	let cart = modalContent[0];
 
-    let cartPrice = modalContent[1]; 
-    
+	let cartPrice = modalContent[1];
+
 	return ReactDOM.createPortal(
 		<div className="flex flex-col h-screen w-screen bg-black/50 place-content-center items-center absolute inset-0">
-			<div className="w-3/6 h-80 border-2 border-slate-400 border-black rounded relative bg-white ">
-				<div className="text-center font-semibold text-2xl pt-2">Your Cart</div>
-				{cart.map((meal) => (
-					<CartItems
-						key={meal.id}
-						name={meal.title}
-						price={meal.price}
-						quantity={meal.quantity}
-					/>
-				))}
+			<div className="w-3/6 h-96 border-2 border-slate-400 border-black rounded relative bg-white ">
+				<div className="text-center font-semibold text-2xl py-2">Your Cart</div>
+				<div className="border border-black h-44 mx-2 rounded-md overflow-y-scroll pt-2 text-center">
+					{cart.length > 0 ? cart.map((meal) => (
+						<CartItems
+							key={meal.id}
+							name={meal.title}
+							price={meal.price}
+							quantity={meal.quantity}
+						/>
+					)) : <p>Your cart is empty, try adding some food! </p>}
+				</div>
 				<div className="flex flex-row font-medium absolute bottom-20 pl-4 space-x-5 text-xl">
 					<div>Total amount:</div>
 					<div>£{cartPrice}</div>
